124a2198e220cef3cdec4ce4bf660db4dcba0b60
GHwcoding <lislam@ucdavis.edu>
2022-05-03 10:42:20 -0700

    Created HTML, CSS, JS files

---
+++ index.js
@@ -0,0 +1,71 @@
+let now = new Date();
+let dayIndex = now.getDay();
+let monthIndex = now.getMonth();
+let date = now.getDate();
+let hour = now.getHours();
+if (hour < 10) {
+  hour = `0${hour}`;
+}
+let minutes = now.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[dayIndex];
+
+let months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+let month = months[monthIndex];
+
+let dateElement = document.querySelector("#date");
+dateElement.innerHTML = `${day}, ${month} ${date} ${hour}:${minutes}`;
+
+function search(city) {
+  let unit = "imperial";
+  let apiKey = "88310d11c396b0c7af7be4b1dfda46f6";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${unit}`;
+  axios.get(apiUrl).then(showWeatherConditions);
+}
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#input-city").value;
+  search(city);
+}
+
+let form = document.querySelector("#enter-city");
+form.addEventListener("submit", handleSubmit);
+
+function showWeatherConditions(response) {
+  document.querySelector("#city-element").innerHTML = response.data.name;
+  document.querySelector("#current-temp").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].main;
+}
+
+search("San Francisco");
